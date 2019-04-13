bfe052b1e561ba2a715ac3fdd7910540d718691b
tmgreenside <trevmg7@gmail.com>
2019-04-13 14:07:51 -0700

    Created group repo

---
+++ Assignment11_Group4.java
@@ -0,0 +1,21 @@
+/*
+names: Trevor Greenside, Thomas Hughes, Matthew Lee
+asgn: Homework 11
+course: CPSC 435 - 01
+date due: 17 April 2019
+*/
+
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.util.*;
+
+class Assignment11_Group4.java {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
