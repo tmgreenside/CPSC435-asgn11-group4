1c8e088467cbd6ca52b4da7b82687bb19eb55530
Matthew Lee <kwwxis@users.noreply.github.com>
2019-04-14 14:21:57 -0700

    Put 'then' on own line and add shebang

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 tester="tgreenside"
 filename="TermPairCount.java"
 classname="TermPairCount"
@@ -8,7 +9,8 @@ testPlatform="Amazon"
 rm -rf $classDirectory
 mkdir $classDirectory
 
-if ["$testPlatform" == "Amazon"] then
+if ["$testPlatform" == "Amazon"]
+then
     javac -classpath /usr/lib/hadoop/client/hadoop-common-2.8.5-amzn-3.jar:/usr/lib/hadoopmapreduce/hadoop-mapreduce-client-core-2.8.5-amzn-3.jar -d $classDirectory $filename
 else
     javac -classpath /share/hadoop-common-2.5.1.jar:/share/hadoop-mapreduce-client-core2.5.1.jar -d $classDirectory $filename
@@ -16,13 +18,13 @@ fi
 
 jar cvf $jarFile -C $classDirectory .
 
-if ["$testPlatform" == "Amazon"] then
+if ["$testPlatform" == "Amazon"]
+then
     /usr/bin/hadoop fs -mkdir /user/"$classname"
     /usr/bin/hadoop fs -mkdir /user/"$classname"/input
     /usr/bin/hadoop fs -put ./input/*.txt /user/"$classname"/input
     /usr/bin/hadoop jar $jarFile $classname /user/"$classname"/input /user/"$classname"/output
     /usr/bin/hadoop fs -cat /user/"$classname"/output/part-00000
-
 else
     hadoop fs -mkdir /user/"$tester" (use your own last name)
     hadoop fs -mkdir /user/"$tester"/termpaircount
