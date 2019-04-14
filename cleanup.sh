ef0c64bb068e438d23ea017913b9b27ec4495dbe
tmgreenside <trevmg7@gmail.com>
2019-04-14 14:25:07 -0700

    Edited cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -1,1 +1,9 @@
-/usr/bin/hadoop fs -rm -r /user/wordcount
+testPlatform="Amazon"
+tester="tgreenside"
+
+if ["$testPlatform" == "Amazon"]
+then
+    /usr/bin/hadoop fs -rm -r /user/wordcount
+else
+    hadoop fs -rmr /user/"$tester"
+fi
